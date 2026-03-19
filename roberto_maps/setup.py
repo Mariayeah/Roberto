7e7fc2df3e5c4f6613c2b9482fac89719d2a78b8
Meryame Ait Boumlik <meryemboumlik612@gmail.com>
2026-03-19 20:03:51 +0000

    feat: implement AMCL localization with map integration and RViz visualization
    Added AMCL configuration, fixed frame alignment, and confirmed /amcl_pose publishing  with correct visualization in RViz. ( using the updated maps )

--- roberto_maps/setup.py
+++ roberto_maps/setup.py
@@ -2,17 +2,18 @@ from setuptools import find_packages, setup
 import os
 from glob import glob
 
-package_name = 'roberto_nav_punto'
+package_name = 'roberto_maps'
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
+    
+data_files=[
     ('share/ament_index/resource_index/packages',
         ['resource/' + package_name]),
     ('share/' + package_name, ['package.xml']),
-    (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    (os.path.join('share', package_name, 'maps'), glob('maps/*')),
 ],
     install_requires=['setuptools'],
     zip_safe=True,
